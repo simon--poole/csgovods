3786f275a44c36f6863eab55e7c07e6d0ed9f1a4
Simon Poole <simon.poole@students.plymouth.ac.uk>
2016-11-25 22:14:02 +0000

    Userbar & teams update

--- gulpfile.js
+++ gulpfile.js
@@ -20,73 +20,75 @@ gulp.task('default', ['build']);
 //Build umbrella task
 gulp.task('build', ['minify-css']);
 
-gulp.task('clean', function () {
-	return gulp.src('dist/')
-		.pipe(clean());
+gulp.task('clean', function() {
+    return gulp.src('dist/')
+        .pipe(clean());
 });
 
-gulp.task('clean-team-icons', function () {
-	return gulp.src('./teams/dist')
-		.pipe(clean());
+gulp.task('clean-team-icons', function() {
+    return gulp.src('./teams/dist')
+        .pipe(clean());
 });
-gulp.task('resize-team-icons', ['clean-team-icons'], function () {
-	return gulp.src(["./teams/**/*.png", "!./teams/dist/*.png"])
-		.pipe(gm(function (gmfile) {
-			return gmfile.resize(48, 20);
-		}))
-		.pipe(gulp.dest("./teams/dist"));
-});
-
-gulp.task('generate-team-icons', ['resize-team-icons'], function () {
-	return sprity.src({
-			src: './teams/dist/**/*.png',
-			style: './less/team-sprites.less',
-			name: 'teams',
-			processor: 'less',
-			margin: 0,
-		})
-		.on('error', gutil.log)
-		.pipe(gulpif('*.png', gulp.dest('./images/'), gulp.dest('./less/')))
+gulp.task('resize-team-icons', ['clean-team-icons'], function() {
+    return gulp.src(["./teams/**/*.png", "!./teams/dist/*.png"])
+        .pipe(gm(function(gmfile) {
+            return gmfile.resize(48, 20);
+        }))
+        .pipe(gulp.dest("./teams/dist"));
 });
 
-gulp.task('generate-thumbnails', ['generate-team-icons'], function () {
-	return sprity.src({
-			src: './thumbs/*.png',
-			style: './less/thumb-sprites.less',
-			name: 'thumbnails',
-			processor: 'less',
-			format: "jpg",
-			margin: 0,
-		})
-		.on('error', gutil.log)
-		.pipe(gulpif('*.jpg', gulp.dest('./images/'), gulp.dest('./less/')))
+gulp.task('generate-team-icons', ['resize-team-icons'], function() {
+    return sprity.src({
+            src: './teams/dist/**/*.png',
+            style: './less/team-sprites.less',
+            name: 'teams',
+            processor: 'less',
+            margin: 0,
+            engine: 'sprity-gm'
+        })
+        .on('error', gutil.log)
+        .pipe(gulpif('*.png', gulp.dest('./images/'), gulp.dest('./less/')))
 });
 
-gulp.task('sprites', ['generate-thumbnails'], function () {
-	return gulp.src(['images/teams.png','images/thumbnails.jpg'])
-		.pipe(imop({
-			optimizationLevel: 7,
-			progressive: true,
-			interlaced: true
-		}))
-		.pipe(gulp.dest('images/'))
+gulp.task('generate-thumbnails', ['generate-team-icons'], function() {
+    return sprity.src({
+            src: './thumbs/*.png',
+            style: './less/thumb-sprites.less',
+            name: 'thumbnails',
+            processor: 'less',
+            format: "jpg",
+            margin: 0,
+            engine: 'sprity-gm'
+        })
+        .on('error', gutil.log)
+        .pipe(gulpif('*.jpg', gulp.dest('./images/'), gulp.dest('./less/')))
 });
 
-gulp.task('less-build', ['clean'], function () {
-	return gulp.src('less/csgovods.less')
-		.pipe(less())
-		.pipe(cssbeautify({
-			indent: '	',
-			openbrace: 'separate-line',
-			autosemicolon: true
-		}))
-		.pipe(gulp.dest('dist/'));
+gulp.task('sprites', ['generate-thumbnails'], function() {
+    return gulp.src(['images/teams.png', 'images/thumbnails.jpg'])
+        // .pipe(imop({
+        //     optimizationLevel: 7,
+        //     progressive: true,
+        //     interlaced: true
+        // }))
+        .pipe(gulp.dest('images/'))
 });
-gulp.task('minify-css', ['less-build'], function () {
-	return gulp.src('dist/csgovods.css')
-		.pipe(cssmin())
-		.pipe(rename({
-			suffix: '.min'
-		}))
-		.pipe(gulp.dest('dist/'));
+
+gulp.task('less-build', ['clean'], function() {
+    return gulp.src('less/csgovods.less')
+        .pipe(less())
+        .pipe(cssbeautify({
+            indent: '	',
+            openbrace: 'separate-line',
+            autosemicolon: true
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+gulp.task('minify-css', ['less-build'], function() {
+    return gulp.src('dist/csgovods.css')
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/'));
 });
